a007a2fb4533193c9e186abf47f6f28e409cd8bf
SammakkoFIN <ventiq.aquilo@gmail.com>
2019-08-12 16:36:29 +0300

    Removed Debug.DrawRay

--- CharacterController2D/Assets/Scripts/CharacterController2D.cs
+++ CharacterController2D/Assets/Scripts/CharacterController2D.cs
@@ -53,8 +53,6 @@ public class CharacterController2D : MonoBehaviour
         // Box cast for collisions
         RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.up * directionY, rayDistance, collisionMask);
         RaycastHit2D rayHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, -Vector2.up, rayDistance, collisionMask);
-        Debug.DrawRay(boxCollider.bounds.center, -Vector2.up * rayDistance * 100);
-
         if (hit)
         {
             ColliderDistance2D colliderDistance = hit.collider.Distance(boxCollider);
